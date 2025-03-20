5960f7161a679d9dae4a49de070ff46ce95344f1
Olabode Ezekiel <ayoeze191@gmail.com>
2025-03-20 11:34:55 +0100

    about us

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from "./pages/landing/Index"
 import About from './pages/about-us'
+import ContactUs from './pages/contact-us/index'
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element ={<Landing />} />
         <Route path="/about-us" element={<About />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+
       </Routes>
       </BrowserRouter>
     </Box>
